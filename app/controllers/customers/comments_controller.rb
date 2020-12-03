e54792bb5e9cb8d26fd042eb8c23079d33fe1ea3
EC2 Default User <ec2-user@ip-172-31-3-155.ap-northeast-1.compute.internal>
2020-12-03 06:05:40 +0000

    install-gem

---
+++ app/controllers/customers/comments_controller.rb
@@ -0,0 +1,4 @@
+class Customers::CommentsController < ApplicationController
+  def create
+  end
+end
---
+++ test/controllers/customers/favorites_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class Customers::FavoritesControllerTest < ActionDispatch::IntegrationTest
+  test "should get create" do
+    get customers_favorites_create_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get customers_favorites_destroy_url
+    assert_response :success
+  end
+
+end
