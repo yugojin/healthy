75264464a239e753bede8cca8e84fe0e8f567c75
EC2 Default User <ec2-user@ip-172-31-13-9.ap-northeast-1.compute.internal>
2020-12-16 05:07:59 +0000

    finish admin

--- app/controllers/admins/customers_controller.rb
+++ app/controllers/admins/customers_controller.rb
@@ -1,10 +1,36 @@
 class Admins::CustomersController < ApplicationController
   def index
+    @customers = Customer.page(params[:id]).per(5)
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+      if @customer.save
+        redirect_to admins_customer_path(@customer)
+      else
+        render 'edit'
+      end
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(
+      :name,
+      :name_kana,
+      :postal_code,
+      :address,
+      :telephone_number,
+      :email,
+      :is_deleted)
   end
 end
--- app/controllers/admins/foods_controller.rb
+++ app/controllers/admins/foods_controller.rb
@@ -1,7 +1,16 @@
 class Admins::FoodsController < ApplicationController
   def index
+    @foods = Food.all
+    @foods = Food.includes(:favorited_customers).sort {|a,b| b.favorited_customers.size <=> a.favorited_customers.size}
   end
 
   def show
+    @food = Food.find(params[:id])
+    @customer = @food.customer
+  end
+
+  private
+  def food_params
+    params.require(:food).permit(:food_name, :image, :introduction, :kcal)
   end
 end
