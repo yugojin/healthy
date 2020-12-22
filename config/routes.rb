bbb0726aab61f1bdd331dc93c7651e9d0300ef9c
EC2 Default User <ec2-user@ip-172-31-13-9.ap-northeast-1.compute.internal>
2020-12-22 04:34:40 +0000

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,36 @@
 Rails.application.routes.draw do
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions',
+  }
+  namespace :admins do
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :foods, only: [:index, :show]
+  end
 
   devise_for :customers, controllers: {
-  sessions: 'customers/sessions',
-  registrations: 'customers/registrations',
-  omniauth_callbacks: 'customers/omniauth_callbacks'
-}
+    sessions: 'customers/sessions',
+    registrations: 'customers/registrations',
+    omniauth_callbacks: 'customers/omniauth_callbacks',
+  }
   root 'customers/homes#top'
   get 'about' => 'customers/homes#about'
+  get "search" => "customers/customers#search"
+  resources :tags do
+    get 'search', to: 'customers/customers#search'
+  end
   namespace :customers do
-    resources :foods,only: [:index, :show, :new, :create] do
-      resources :comments, only: [:create]
-      resources :favorites, only: [:create, :destroy]
+    resources :foods, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
+      resources :comments, only: [:create, :destroy]
+      resource :favorites, only: [:create, :destroy]
     end
 
-    resources :customers,only: [:show, :edit, :update] do
-	    member do
+    resources :customers, only: [:show, :edit, :update] do
+      member do
         get 'withdrawal'
         patch 'withdrawal' => 'customers#change', as: "change"
       end
-  	end
+    end
   end
 
-  devise_for :admins, controllers: {
-    sessions: 'admins/sessions'
-  }
-  get 'top'=>'customers#top'
-  namespace :admins do
-    resources :customers,only: [:index, :show, :edit, :update]
-    resources :foods,only: [:index, :show]
-  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
