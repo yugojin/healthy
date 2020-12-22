22f75a10c21454993ce42db5a877e4e6da2e13b6
EC2 Default User <ec2-user@ip-172-31-13-9.ap-northeast-1.compute.internal>
2020-12-22 12:07:23 +0000

    fix migrate

--- db/migrate/20201203034156_create_comments.rb
+++ db/migrate/20201203034156_create_comments.rb
@@ -2,6 +2,8 @@ class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
       t.text :comment_content
+      t.references :customer, foreign_key: true
+      t.references :food, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20201211042543_add_customer_id_to_comments.rb
+++ db/migrate/20201211042543_add_customer_id_to_comments.rb
@@ -1,6 +1,4 @@
 class AddCustomerIdToComments < ActiveRecord::Migration[5.2]
   def change
-    add_column :comments, :customer_id, :integer
-    add_column :comments, :food_id, :integr
   end
 end
